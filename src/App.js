d9181695808554b9b4b1aa2d1d560c091823e4af
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-14 15:50:02 +0600

    🛠️💅📕layout updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import About from "./Components/About";
 import Decrypt from "./Components/Decrypt";
 import Encrypt from "./Components/Encrypt";
 import Header from "./Components/Header";
 
 export default function App() {
+  const [Tab, setTab] = useState("encrypt");
+  const tabs = [
+    {
+      name: "Encrypt",
+      handle: "encrypt",
+      component: <Encrypt />,
+    },
+    {
+      name: "Decrypt",
+      handle: "decrypt",
+      component: <Decrypt />,
+    },
+  ];
   return (
     <Container>
       <Header />
       <main>
-        <About />
-        <Encrypt />
-        <Decrypt />
+        <nav>
+          {React.Children.toArray(
+            tabs.map((tab) => (
+              <button
+                className={`item ${Tab === tab.handle ? "active" : ""}`}
+                onClick={() => setTab(tab.handle)}
+              >
+                {tab.name}
+              </button>
+            ))
+          )}
+        </nav>
+        {{ encrypt: <Encrypt />, decrypt: <Decrypt /> }[Tab]}
       </main>
     </Container>
   );
@@ -23,10 +45,38 @@ const Container = styled.div`
   margin: auto;
   & > main {
     padding: 24px;
+    padding-top: 0;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     gap: 24px;
+    & > nav {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 24px;
+      & > button {
+        background-color: transparent;
+        color: var(--white);
+        border: none;
+        outline: none;
+        &.active {
+          color: var(--light);
+          position: relative;
+          &::after {
+            content: "";
+            position: absolute;
+            height: 2px;
+            bottom: 0;
+            left: 50%;
+            background-color: var(--light);
+            transform: translateX(-50%);
+            width: 115%;
+            /* transform-origin: left; */
+          }
+        }
+      }
+    }
   }
 `;
