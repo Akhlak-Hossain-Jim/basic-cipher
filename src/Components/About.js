31e37ed658b7d2d4a390b2386e7de87462b587d0
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-14 15:50:12 +0600

    💅updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 export default function About() {
   const [Sentence, setSentence] = useState("");
+  let sentence =
+    "This is a basic cipher functionality to encrypt or decrypt sentences in simple letter rotation manner.";
   useEffect(() => {
     let Step = 0;
     let s = "";
-    let sentence =
-      "This is a basic cipher functionality to encrypt or decrypt sentences in simple rotation manner.";
     let IID = setInterval(() => {
       if (Step < sentence.length) {
         s += sentence[Step];
@@ -20,17 +20,31 @@ export default function About() {
 
   return (
     <Container>
+      <div className="back">{sentence}</div>
       <p>{Sentence}</p>
     </Container>
   );
 }
 
 const Container = styled.section`
-  width: min(600px, 100%);
+  width: min(700px, 100%);
   margin: auto;
+  position: relative;
+  & > .back {
+    text-align: center;
+    opacity: 0;
+    line-height: 120%;
+    font-size: 0.8rem;
+  }
   & > p {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     text-align: center;
     color: var(--light);
-    line-height: 140%;
+    line-height: 120%;
+    font-size: 0.8rem;
   }
 `;
